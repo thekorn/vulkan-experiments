a341429dfbd616f8d1a62b2d3528311efc6ccefd
Markus Korn <markus.korn@gmail.com>
2024-06-28 07:23:22 +0200

    Enhance Vulkan handling and documentation
    - Enhance README.md with setup and troubleshooting instructions for Vulkan API
    - Introduce new Vulkan extension handling function in main.zig to improve cross-platform compatibility
    - Update dynamic library loading order for Apple platforms in main.zig

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # vulkan experiments
 
+## running
+
+Run the application
+```bash
+zig build run
+```
+
+Run the application and debug print the vulkan api calls
+```bash
+VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump zig build run
+```
+
+Alternatively, use `vkconfig` for more fine grained layers config.
+
 
 ## ressources
 - [Vulkan Tutorial](https://vulkan-tutorial.com/)
--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,28 @@ const PFN_vkGetInstanceProcAddr = std.meta.Child(c.PFN_vkGetInstanceProcAddr);
 const PFN_vkCreateInstance = std.meta.Child(c.PFN_vkCreateInstance);
 const PFN_vkDestroyInstance = std.meta.Child(c.PFN_vkDestroyInstance);
 
+fn getExtensionNames() []const [*:0]const u8 {
+    return switch (buildin.os.tag) {
+        // see: https://vulkan.lunarg.com/doc/sdk/1.3.283.0/mac/getting_started.html
+        // section `Common Problems - Encountered VK_ERROR_INCOMPATIBLE_DRIVER`
+        .ios, .macos, .tvos, .watchos => &[_][*:0]const u8{
+            c.VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME,
+            c.VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+        },
+        else => &.{},
+    };
+}
+
 fn getLibrary() !std.DynLib {
+    // logic taken from `volk` library (https://github.com/zeux/volk)
     return switch (buildin.os.tag) {
         .windows => std.DynLib.open("vulkan-1.dll"),
         .ios, .macos, .tvos, .watchos => {
-            const LibraryNames = [_][]const u8{ "libMoltenVK.dylib", "libvulkan.dylib", "libvulkan.1.dylib" };
+            const LibraryNames = [_][]const u8{
+                "libvulkan.dylib",
+                "libvulkan.1.dylib",
+                "libMoltenVK.dylib",
+            };
             for (LibraryNames) |name| {
                 const lib = std.DynLib.open(name) catch continue;
                 return lib;
@@ -47,7 +64,14 @@ const Vulkan = struct {
     allocation_callbacks: ?*c.VkAllocationCallbacks,
 
     fn init(entry: Entry) !Self {
-        const info = c.VkInstanceCreateInfo{ .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+        const extensions = getExtensionNames();
+
+        const info = c.VkInstanceCreateInfo{
+            .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .flags = c.VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR,
+            .enabledExtensionCount = @intCast(extensions.len),
+            .ppEnabledExtensionNames = extensions.ptr,
+        };
         const create_instance: PFN_vkCreateInstance = @ptrCast(entry.get_instance_proc_addr(null, "vkCreateInstance"));
         var instance: c.VkInstance = undefined;
         const allocation_callbacks: ?*c.VkAllocationCallbacks = null;
